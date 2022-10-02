7a461b1a06fd46093f80584d308fa32bfb8bd345
Bogdan-M-source <bogdan.muntean98@gmail.com>
2022-10-02 23:14:46 +0300

    ex1 edabit

---
+++ Edabit-Exercises/exercises1.js
@@ -0,0 +1,12 @@
+// 1. Return the Sum of Two Numbers
+// Create a function that takes two numbers as arguments and returns their sum.
+
+// let var1 = 3;
+// let var2 = 5;
+// function addition(a,b) {
+//     return a + b;
+// }
+// console.log(addition(var1,var2))
+
+//2. Find the Perimeter of a Rectangle
+// Create a function that takes length and width and finds the perimeter of a rectangle.
