0870b94f64d01fb015bd6c32f76f4440cdd000aa
Bogdan-M-source <bogdan.muntean98@gmail.com>
2022-10-03 12:18:22 +0300

    ex 3,4,5 edabit

--- Edabit-Exercises/exercises1.js
+++ Edabit-Exercises/exercises1.js
@@ -7,17 +7,55 @@
 //     return a + b;
 // }
 // console.log(addition(var1,var2))
-
+//  ----------------------------------------------------
 // 2. Area of a Triangle
 // Write a function that takes the base and height of a triangle and return its area.
 
 // let base = 3;
 // let height = 5;
-
-// function triangle(a,b) {
+// function triangleArea(a,b) {
 //     return ( a * b ) / 2;
 // }
-// console.log(triangle( base, height ))
+// console.log(triangleArea( base, height ))
+//  ----------------------------------------------------
+// 3. Return Something to Me!
+// Write a function that returns the string "something" joined with a space " " and the given argument a.
 
+// let word = "thing";
+
+// function somethingToMe( a ) {
+//     return "something " + a;
+//     // return `something ${a}`;
+// }
+// console.log( somethingToMe(word) );
+//  ----------------------------------------------------
+// 4. Convert Age to Days
+// Create a function that takes the age in years and returns the age in days.
+// let var1 = 20;
+
+// function yearsToDays( year ) {
+//     return `${year} years represent ${year * 360} days`;
+// }
+// console.log(yearsToDays(var1))
+//  ----------------------------------------------------
+// 5.Sum of Polygon Angles
+// Given an n-sided regular polygon n, return the total sum of internal angles (in degrees).
+
+let numberOfSides = 6;
+
+function sumOfPolygonAngles( sides ) {
+    return (sides - 2) * 180;
+}
+console.log(sumOfPolygonAngles( numberOfSides ))
+
+
+
+//  ----------------------------------------------------
+
+//  ----------------------------------------------------
+
+//  ----------------------------------------------------
+
+//  ----------------------------------------------------
 // Find the Perimeter of a Rectangle
 // Create a function that takes length and width and finds the perimeter of a rectangle.
