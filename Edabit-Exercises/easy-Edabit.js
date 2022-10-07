94ce4e4b2b2a05aa43395bc320fc346795d2482b
Bogdan-M-source <bogdan.muntean98@gmail.com>
2022-10-07 21:46:57 +0300

    to resolve ex 4 easy

--- Edabit-Exercises/easy-Edabit.js
+++ Edabit-Exercises/easy-Edabit.js
@@ -31,36 +31,53 @@
 //  -----------------------------------------
 // 3. Matchstick Houses
 //input from terminal
-const readline = require("prompt-sync");
-const prompt = readline()
-let input = prompt("Enter the value: ");
-console.log(`Your value is ${input} and has this result: ${matchStick(input)}`)
+// const readline = require("prompt-sync");
+// const prompt = readline()
+// let input = prompt("Enter the value: ");
+// console.log(`Your value is ${input} and has this result: ${matchStick(input)}`)
 
-let test1 = 0;
-let test2 = 4;
-let test3 = 25;
-let test4 = 81;
+// let test1 = 0;
+// let test2 = 4;
+// let test3 = 25;
+// let test4 = 81;
 
 
-function matchStick( number ){
-    let sticks = 0;
-    if (number === 0) return 0;
-    for( let i = 1; i <= number; i++ ){
-        if( i == 1 ){ 
-            sticks += 6; 
-        } else if ( i != 1 ){
-            sticks += 5;
-        }
-        // console.log(sticks)
-    }
-    return sticks;
-}
+// function matchStick( number ){
+//     let sticks = 0;
+//     if (number === 0) return 0;
+//     for( let i = 1; i <= number; i++ ){
+//         if( i == 1 ){ 
+//             sticks += 6; 
+//         } else if ( i != 1 ){
+//             sticks += 5;
+//         }
+//         // console.log(sticks)
+//     }
+//     return sticks;
+// }
 
 // console.log( matchStick(test1))
 // console.log( matchStick(test2))
 // console.log( matchStick(test3))
 
 //  -----------------------------------------
+// 4. Left Shift by Powers of Two 
+const readline = require("prompt-sync");
+const prompt = readline()
+
+let number = prompt("Enter the number: ");
+let power = prompt("Enter the power of two: ");
+
+function shiftToLeft(x, y){
+    if(y === 1)
+        return 2;
+    else if(y === 0)
+        return 1
+    console.log(y)
+    return x * shiftToLeft(2, y-1);
+}
+console.log(`Your result is: ${shiftToLeft(number, power)}`);
+
 //  -----------------------------------------
 //  -----------------------------------------
 //  -----------------------------------------
