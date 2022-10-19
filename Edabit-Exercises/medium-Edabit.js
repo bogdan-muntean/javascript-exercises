1ee8e3489d72fb60c99a33ddf90192276366e29f
Bogdan-M-source <bogdan.muntean98@gmail.com>
2022-10-19 10:33:40 +0300

    ex 5 medium

--- Edabit-Exercises/medium-Edabit.js
+++ Edabit-Exercises/medium-Edabit.js
@@ -38,25 +38,49 @@
 // dice  1-6 possible of advance positions
 // position player 1 and 2  a and b 
 
-function possibleBonus(a, b){
-    if(b-a >= 1 && b-a <= 6 && a > 0 && b > 0){
-        return true;
-    } else if(a === b || b > a){
-        return false
-    } else {
-        return false;
-    }
-}
+// function possibleBonus(a, b){
+//     if(b-a >= 1 && b-a <= 6 && a > 0 && b > 0){
+//         return true;
+//     } else if(a === b || b > a){
+//         return false
+//     } else {
+//         return false;
+//     }
+// }
 
-console.log(possibleBonus(3, 7)) 
-console.log(possibleBonus(1, 9)) 
-console.log(possibleBonus(5, 3)) 
+// console.log(possibleBonus(3, 7)) 
+// console.log(possibleBonus(1, 9)) 
+// console.log(possibleBonus(5, 3)) 
 
 // possibleBonus(3, 7) ➞ true
 // possibleBonus(1, 9) ➞ false
 // possibleBonus(5, 3) ➞ false
 
 //   ----------------------------------------
+// 5. Right Shift by Division
+// The right shift operation is similar to floor division by powers of two.
+// Sample calculation using the right shift operator ( >> ):
+function shiftToRight(a, b){
+    let finalResultB = 1;
+    for(let i = 1; i <= b; i++){
+        finalResultB = finalResultB*2;
+    }
+    return Math.floor(a/finalResultB);
+}
+
+console.log(shiftToRight(80, 3))
+console.log(shiftToRight(-24, 2))
+console.log(shiftToRight(-5, 1))
+console.log(shiftToRight(4666, 6))
+console.log(shiftToRight(3777, 6))
+console.log(shiftToRight(-512, 10))
+
+// shiftToRight(80, 3) ➞ 10
+// shiftToRight(-24, 2) ➞ -6
+// shiftToRight(-5, 1) ➞ -3
+// shiftToRight(4666, 6) ➞ 72
+// shiftToRight(3777, 6) ➞ 59
+// shiftToRight(-512, 10) ➞ -1
 //   ----------------------------------------
 //   ----------------------------------------
 //   ----------------------------------------
--- Edabit-Exercises/tempCodeRunnerFile.js
+++ Edabit-Exercises/tempCodeRunnerFile.js
@@ -1,19 +1,6 @@
-//         return result;
-//     }
-// }
-
-// const f3 = redundant("qqq")
-// console.log(f3())
-
-//   ----------------------------------------
-// 3. RegEx Exercise: An empty string
-
-// const REGEXP = /(?!=.*[\s\d\w])/g
-// const REGEXP = /^\s*$/
-// 
-// console.log(REGEXP.test("test")) 
-// console.log(REGEXP.test("")) 
-
-// REGEXP.test("") ➞ true
-
-//   ----------------------------------------
+console.log(shiftToRight(80, 3))
+// console.log(shiftToRight(-24, 2))
+// console.log(shiftToRight(-5, 1))
+// console.log(shiftToRight(4666, 6))
+// console.log(shiftToRight(3777, 6))
+// console.log(shiftToRight(-512, 10))
