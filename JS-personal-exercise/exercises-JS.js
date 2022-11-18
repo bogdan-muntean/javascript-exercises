f8d996ee5702779c6becdba4f6d27b6b9b3b4b2e
Bogdan-M-source <bogdan.muntean98@gmail.com>
2022-11-18 10:51:29 +0200

    correct the ex 6

--- JS-personal-exercise/exercises-JS.js
+++ JS-personal-exercise/exercises-JS.js
@@ -72,25 +72,31 @@
 function same (arr, test) {
     //1.cum descriu problema cu cuvintele proprii
     //'verificarea frecventei pentru patratul elementelor'
-    //2.Putem introduce arr de numere
-    //3.Afisam boolean, are aceeasi frecventa sau nu
+    //2.input = arr de numere
+    //3.ouput = boolean, are aceeasi frecventa sau nu
     //4.pot determina ce se va afisa, cu ajutorul input-ului? Da
     //5.Cum denumesc importante parti de code din problema?
-    //un obiect 'de comparat', in care fiecare element din arr1 va fi patratul lui arr1[x] de pe acea pozitie
+    //un obiect 'de comparat'
     let counter = {}
-    let testObject = {}
+    if(arr.length !== test.length){
+        return false;
+    }
     for(let i = 0; i < arr.length; i++){
         let patratul = arr[i] * arr[i];
+        // let patratul = arr[i] ** 2;
         counter[patratul] > 0 ? ++counter[patratul] : counter[patratul] = 1;
     }
     for(let i = 0; i < test.length; i++){
-        testObject[test[i]] ? ++testObject[test[i]] : testObject[test[i]] = 1;
+        let element = test[i];
+        if(!counter[element]){
+            return false
+        }  else {
+            counter[element]--;
+        }
     }
-    console.log(counter, testObject)
-    if(counter.value === testObject.value){
-        return true;
-    } else {
-        return false;
-    }
+    return true;
+    
 }
 console.log(same([3,7,5], [9, 49, 25]))
+console.log(same([3,7,5], [2, 3, 10]))
+console.log(same([3,7,5], [7, 49, 25]))
